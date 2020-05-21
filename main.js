9112ef1a0aa65a6ffdbea6a2ce839fcbc59de326
Federico Rachelli <chello@Foxtrot.homenet.telecomitalia.it>
2020-05-21 13:16:56 +0200

    json-editor now works

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 // Modules to control application life and create native browser window
 const {app, BrowserWindow} = require('electron')
 const path = require('path')
+const fs = require('fs')
+
 
 function createWindow () {
   // Create the browser window.
@@ -16,7 +18,8 @@ function createWindow () {
   mainWindow.loadFile('index.html')
 
   // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+  mainWindow.webContents.openDevTools()
+
 }
 
 // This method will be called when Electron has finished
--- preload.js
+++ preload.js
@@ -1,5 +1,4 @@
-// All of the Node.js APIs are available in the preload process.
-// It has the same sandbox as a Chrome extension.
+
 window.addEventListener('DOMContentLoaded', () => {
   const replaceText = (selector, text) => {
     const element = document.getElementById(selector)
@@ -9,4 +8,69 @@ window.addEventListener('DOMContentLoaded', () => {
   for (const type of ['chrome', 'node', 'electron']) {
     replaceText(`${type}-version`, process.versions[type])
   }
+
+  //const {JSONEditor} = require('@json-editor/json-editor')
+  console.log(JSONEditor);
+
+  var starting_value = [
+    {
+      name: "John Smith",
+      age: 35,
+      gender: "male",
+      location: {
+        city: "San Francisco",
+        state: "California",
+        citystate: ""
+      },
+      pets: [
+        {
+          name: "Spot",
+          type: "dog",
+          fixed: true
+        },
+        {
+          name: "Whiskers",
+          type: "cat",
+          fixed: false
+        }
+      ]
+    }
+  ];
+  
+  //const element = document.getElementById('editor_holder');
+  var editor = new JSONEditor(document.getElementById('editor_holder'),{
+    // Enable fetching schemas via ajax
+    ajax: true,
+    
+    // The schema for the editor
+    schema: {
+      type: "array",
+      title: "People",
+      format: "tabs",
+      items: {
+        title: "Person",
+        headerTemplate: "{{i}} - {{self.name}}",
+        oneOf: [
+          {
+            $ref: "basic_person.json",
+            title: "Basic Person"
+          },
+          {
+            $ref: "person.json",
+            title: "Complex Person"
+          }
+        ]
+      }
+    },
+    
+    // Seed the form with a starting value
+    startval: starting_value,
+    
+    // Disable additional properties
+    no_additional_properties: true,
+    
+    // Require all properties by default
+    required_by_default: true
+  });
+
 })
