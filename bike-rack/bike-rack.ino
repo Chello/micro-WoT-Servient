3532554008973a9cac23695e3b67e5b11974c4c5
Federico Rachelli <chello@Foxtrot.homenet.telecomitalia.it>
2020-09-05 17:07:57 +0200

    Array set

--- bike-rack/bike-rack.ino
+++ bike-rack/bike-rack.ino
@@ -23,17 +23,23 @@ DeserializationError err;
 
 int properties_number = 1;
 int objectProperties_number = 0;
-int actions_number = 0;
+int actions_number = 1;
 int events_number = 0;
 
 // Properties
 const char* property1_name = "parks";
+DynamicJsonDocument property1_jdoc(300);
 JsonArray property1_value;
 
 
+// Actions
+const char* action1_name = "isParkFree";
+int action1_inputsNumber = 1;
+String action1_schema[1] = {"{\"name\":\"rack_num\",\"type\":\"integer\"}"};
 
 
 // Endpoints
+String req5 = "/" + thingName + "/actions/" + action1_name;
 String req4 = "/" + thingName + "/properties/" + property1_name;
 String req3 = "/" + thingName + "/all/properties";
 String req2 = "/" + thingName;
@@ -51,11 +57,12 @@ String request1();
 String request2();
 String request3();
 String request4();
+String request5(String body);
 
 //HTTP - actions
-const int http_actions_num = 0;
-const String http_actions_endpoint[http_actions_num] = {  };
-actions_handler http_actions_callback[http_actions_num] = {  };
+const int http_actions_num = 1;
+const String http_actions_endpoint[http_actions_num] = { req5 };
+actions_handler http_actions_callback[http_actions_num] = { request5 };
 
 //WS - actions
 const int ws_actions_num = 0;
@@ -85,7 +92,7 @@ void setup() {
   
     connection(ssid, password);
     
-    td = "{\"title\":\"bike-rack\",\"id\":\"bike-rack\",\"@context\":[\"https://www.w3.org/2019/wot/td/v1\"],\"security\":\"nosec_sc\",\"securityDefinitions\":{\"nosec_sc\":{\"scheme\":\"nosec\"}},\"forms\":[{\"contentType\":\"application/json\",\"href\":\""+urlServer+"/all/properties\",\"op\":[\"readallproperties\",\"readmultipleproperties\"]},{\"contentType\":\"application/json\",\"href\":\""+urlSocket+"/all/properties\",\"op\":[\"readallproperties\",\"readmultipleproperties\"]}],\"links\":[],\"properties\":{\"parks\":{\"forms\":[{\"contentType\":\"application/json\",\"href\":\""+urlServer+"/properties/"+property1_name+"\",\"op\":[\"readproperty\"]},{\"contentType\":\"application/json\",\"href\":\""+urlSocket+"/properties/"+property1_name+"\",\"op\":[\"readproperty\"]}],\"type\":\"array\",\"items\":{\"type\":\"boolean\"},\"observable\":false,\"readOnly\":true,\"writeOnly\":true}},\"actions\":{},\"events\":{}}";
+    td = "{\"title\":\"bike-rack\",\"id\":\"bike-rack\",\"@context\":[\"https://www.w3.org/2019/wot/td/v1\"],\"security\":\"nosec_sc\",\"securityDefinitions\":{\"nosec_sc\":{\"scheme\":\"nosec\"}},\"forms\":[{\"contentType\":\"application/json\",\"href\":\""+urlServer+"/all/properties\",\"op\":[\"readallproperties\",\"readmultipleproperties\"]},{\"contentType\":\"application/json\",\"href\":\""+urlSocket+"/all/properties\",\"op\":[\"readallproperties\",\"readmultipleproperties\"]}],\"links\":[],\"properties\":{\"parks\":{\"forms\":[{\"contentType\":\"application/json\",\"href\":\""+urlServer+"/properties/"+property1_name+"\",\"op\":[\"readproperty\"]},{\"contentType\":\"application/json\",\"href\":\""+urlSocket+"/properties/"+property1_name+"\",\"op\":[\"readproperty\"]}],\"type\":\"array\",\"items\":{\"type\":\"boolean\"},\"observable\":false,\"readOnly\":true,\"writeOnly\":true}},\"actions\":{\"isParkFree\":{\"forms\":[{\"contentType\":\"application/json\",\"href\":\""+urlServer+"/actions/"+action1_name+"\",\"op\":\"invokeaction\"}],\"input\":{\"rack_num\":{\"type\":\"integer\"}},\"output\":{\"type\":\"boolean\"},\"safe\":true,\"idempotent\":false}},\"events\":{}}";
 
     hlp = new embeddedWoT_HTTP_LongPoll(portServer);
 
@@ -103,7 +110,15 @@ void setup() {
     Serial.println("Server started");
     Serial.println(urlServer);
 
-    
+    property1_value = property1_jdoc.to<JsonArray>();
+
+    // property1_value.add(false);
+    // property1_value.add(false);
+    // property1_value.add(false);
+    property1_value[0] = false;	
+    property1_value[1] = false;
+    property1_value[2] = false;
+	
 }    
 
 void loop() {
@@ -147,13 +162,16 @@ String request2() {
 }
 
 String request3() {
-    DynamicJsonDocument tmp(20);
+    DynamicJsonDocument tmp(2000);
     String resp = "";
     JsonObject obj = tmp.createNestedObject();
 
     Serial.println("\nGET all properties");
+    
     obj[property1_name] = property1_value;
     serializeJson(obj, resp);
+    serializeJsonPretty(obj, Serial);
+
 
     return resp;
 }
@@ -169,4 +187,92 @@ String request4() {
     return resp;
 }
 
+String request5(String body) {
+    DynamicJsonDocument resp_doc(200);
+    String resp = "";
+
+    Serial.printf("\nPOST invokeaction %s\n", action1_name);
+    Serial.printf("Body received: %s\n", body.c_str());
+    
+    err = deserializeJson(resp_doc, body);
+    if(err) {
+        Serial.printf("deserializeJson() failed with code %s", err.c_str());
+        resp = err.c_str();
+        return resp;
+    }
+    else {
+        if(resp_doc["rack_num"].isNull())
+            resp = "InvalidInput";
+        else {
+            bool validInput = true;
+            String value = "";
+
+            String action1_input[1] = {};    
+            int action1_input1_value = 0;
+
+            i = 0;
+            while(validInput and i<action1_inputsNumber) {
+                switch(i) {
+                    case 0: {
+                        value = "";
+                        serializeJson(resp_doc["rack_num"], value);
+                        action1_input[0] = value;
+                        validInput = handleInputType(value,action1_schema[0]);
+                    }
+                    break;
+
+                }
+                i++;
+            }    
+
+            if(validInput) {
+
+                action1_input1_value = action1_input[0].toInt();
+
+                bool output = isParkFree(action1_input1_value);    
+                resp = (String) output;
+            }
+            else
+                resp = "InvalidInput";
+        }
+    }
+    return resp;
+}
+
+// handle Input Types
+bool handleInputType(String value, String schema) {   
+	DynamicJsonDocument schema_doc(200);
+    bool validInput = true;
+
+    deserializeJson(schema_doc, schema);
+    JsonObject obj_schema = schema_doc.as<JsonObject>();
+    String type = obj_schema["type"];
+    
+    if(value[0] == '"')
+        value.remove(0);
+    if(value[value.length()-1] == '"')    
+        value.remove(value.length()-1);
+    
+		if(type.equals("integer") || type.equals("number")) {
+        int dot_count = 0;
+        i = 0;
+        while(validInput && i<value.length()) {
+            if(!isDigit(value[i])) 
+                validInput = false;
+            else if(value[i] == '.')
+                if(i == 0 || i == value.length()-1 || dot_count > 1)
+                    validInput = false;
+                else 
+                    dot_count++;    
+            i++;          
+        } 
+    }
+    return validInput;
+}
+
+// Action functions
+bool isParkFree(int rack_num) {
+	return property1_value[rack_num].as<bool>();
+	
+}
 
